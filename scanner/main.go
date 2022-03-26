93d78456c006eef4097e6fa4c454153d5168ab8a
jazz-e <ritchiejohnson1970@gmail.com>
2022-03-26 19:49:59 +0000

    scanner updated

--- scanner/main.go
+++ scanner/main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 func main() {
+	var wg sync.WaitGroup
 	for i := 1; i <= 1024; i++ {
+		wg.Add(1)
 		go func(j int) {
+			defer wg.Done()
 			address := fmt.Sprintf("scanme.nmap.org:%d", j)
 			conn, err := net.Dial("tcp", address)
 
@@ -17,7 +21,9 @@ func main() {
 			}
 
 			conn.Close()
+
 			fmt.Printf("%d open\n", j)
 		}(i)
 	}
+	wg.Wait()
 }
